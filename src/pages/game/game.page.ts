aea2b7bff9d3ab29074fefe37af6e78ddde1a850
Ricardo Silva <rikpiro@gmail.com>
2016-10-26 19:37:00 +0100

    Created basic app skeleton and first navigation

---
+++ src/pages/game/game.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'game',
+  templateUrl: 'game.page.html'
+})
+export class GamePage {
+
+  constructor(public navCtrl: NavController) {}
+}
---
+++ src/pages/team-detail/team-detail.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'team-detail',
+  templateUrl: 'team-detail.page.html'
+})
+export class TeamDetailPage {
+
+  constructor(public navCtrl: NavController) {}
+}
